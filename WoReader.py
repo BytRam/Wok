db6c5b93392d90d5fee09edff0e1323c4c3c7b4a
BytRam <47318239+BytRam@users.noreply.github.com>
2024-04-26 05:22:49 +0300

    Update WoReader.py
    A QoS update so as not to get bored from the waiting time.

--- WoReader.py
+++ WoReader.py
@@ -1,19 +1,19 @@
 import spacy
 from collections import Counter
 
-# Load English language model
+# Loads English language model
 nlp = spacy.load("en_core_web_sm")
 
-# Define a Counter to store word frequencies
+# Defines a Counter to store word frequencies
 word_freq = Counter()
 
-# Process text from a file line by line
+# Processes text from a file line by line
 with open("random_paragraphs.txt", "r") as file:
     for i, line in enumerate(file):
-        # Process each line with spaCy
+        # Processes each line with spaCy
         doc = nlp(line)
 
-        # Remove stop words and punctuation, and count word frequencies
+        # Removes stop words and punctuation, and count word frequencies
         for token in doc:
             if not token.is_stop and not token.is_punct:
                 word_freq[token.text.lower()] += 1
@@ -22,6 +22,6 @@ with open("random_paragraphs.txt", "r") as file:
         if (i + 1) % 100 == 0:
             print(f"Processed {i + 1} lines...")
 
-# Display word frequency count
+# Displays word frequency count
 for word, freq in word_freq.most_common():
     print(f"{word}: {freq}")
